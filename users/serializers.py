88f9263b33006870d5129e445a233fd047f40260
Арсений Карпов <arsenijkarpov@Karsen146.local>
2021-10-24 21:12:30 +0300

    Overriding create in UserTokenSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -3,6 +3,10 @@ from rest_framework import serializers
 
 
 class UserTokenSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        user = UserToken.objects.create_user(**validated_data)
+        return user
+
     class Meta:
         model = UserToken
         fields = '__all__'
