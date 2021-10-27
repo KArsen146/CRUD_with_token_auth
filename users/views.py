9991931e4d11bce53fcf26a574cf043f426d490a
Арсений Карпов <arsenijkarpov@Karsen146.local>
2021-10-27 14:43:25 +0300

    Mv IsauthenticatedWithoutCreate to permissions.py

--- users/views.py
+++ users/views.py
@@ -4,18 +4,11 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
 from users.models import UserToken
+from users.permissions import IsAuthenticatedWithoutCreate
 from users.serializers import UserTokenSerializer
-from rest_framework.permissions import IsAuthenticated
 
 
 class UserTokenViewSet(ModelViewSet):
-    class IsAuthenticatedWithoutCreate(IsAuthenticated):
-
-        def has_permission(self, request, view):
-            if request.method == 'POST':
-                return True
-            super().has_permission(request, view)
-
     serializer_class = UserTokenSerializer
     queryset = UserToken.objects.all()
     permission_classes = (IsAuthenticatedWithoutCreate, )
